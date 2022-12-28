25b7f9df9fd351983aaadf99d063c6975d512390
ababoum <ababou237@hotmail.com>
2022-12-28 16:05:06 +0100

    day00 wip ex04

--- module00/ex01/TinyStatistician.py
+++ module00/ex01/TinyStatistician.py
@@ -1,5 +1,6 @@
 '''Statistics functions'''
 
+import numbers
 import numpy as np
 
 class TinyStatistician:
@@ -9,55 +10,55 @@ class TinyStatistician:
 
     def mean(self, x):
         '''Computes the mean value of a list of numbers'''
-        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int)) for item in x ):
+        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int, numbers.Number)) for item in x ):
             print("Function 'mean' takes only a list or an array of numbers as parameter")
             return None
-        if not x:
+        if len(x) < 1:
             return None
         total = 0.0
         for item in x:
             total += item
-        return total / len(x)
+        return float(total / len(x))
 
     def median(self, x):
         '''Computes the median value of a list of numbers'''
-        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int)) for item in x ):
+        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int, numbers.Number)) for item in x ):
             print("Function 'median' takes only a list or an array of numbers as parameter")
             return None
-        if not x:
+        if len(x) < 1:
             return None
-        y = x
+        y = list(x)
         y.sort()
         if len(y) % 2 == 1:
-            return y[len(y) // 2]
-        return (y[len(y) // 2] + y[-1 + len(y) // 2]) / 2
+            return float(y[len(y) // 2])
+        return float((y[len(y) // 2] + y[-1 + len(y) // 2]) / 2)
 
     def quartile(self, x):
         '''Computes the first and third quartiles of a list of numbers'''
-        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int)) for item in x ):
+        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int, numbers.Number)) for item in x ):
             print("Function 'quartile' takes only a list or an array of numbers as parameter")
             return None
-        if not x:
+        if len(x) < 1:
             return None
-        y = x
+        y = list(x)
         y.sort()
         return [self.percentile(x, 25), self.percentile(x, 75)]
 
     def percentile(self, x, p):
         '''Computes the expected percentile of a list of numbers'''
-        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int)) for item in x ):
+        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int, numbers.Number)) for item in x ):
             print("Function 'percentile' takes only a list or an array of numbers as parameter")
             return None
         if p not in range(0, 101):
             print("The wished percentile must be between 0 and 100")
             return None
-        if not x:
+        if len(x) < 1:
             return None
-        y = x
+        y = list(x)
         y.sort()
         res = float(p * (len(y) - 1) / 100)
-        lim1 = round(res)
-        lim2 = round(res) + 1
+        lim1 = int(res)
+        lim2 = int(res) + 1
 
         if res.is_integer():
             return float(y[lim1])
@@ -66,23 +67,22 @@ class TinyStatistician:
 
     def var(self, x):
         '''Computes the variance of a list of numbers'''
-        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int)) for item in x ):
+        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int, numbers.Number)) for item in x ):
             print("Function 'var' takes only a list or an array of numbers as parameter")
             return None
-        if not x:
+        if len(x) <= 1:
             return None
-        return sum((item - self.mean(x)) ** 2 for item in x) / len(x)
+        return float(sum((item - self.mean(x)) ** 2 for item in x) / (len(x) - 1))
 
     def std(self, x):
         '''Computes the standard deviation of a list of numbers'''
-        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int)) for item in x ):
+        if not isinstance(x, (list, np.ndarray)) or any(not isinstance(item, (float, int, numbers.Number)) for item in x ):
             print("Function 'var' takes only a list or an array of numbers as parameter")
             return None
         var = self.var(x)
         if not var:
             return None
-        else:
-            return var ** 0.5
+        return var ** 0.5
 
 
 if __name__ == "__main__":
@@ -101,6 +101,16 @@ if __name__ == "__main__":
     # Expected result: [10.0, 59.0]
     print(tstat.quartile(b))
     # Expected result: [5.0, 89.0]
+
+    print('*' * 25)
+    print(tstat.percentile(a, 10))
+    # 4.6
+    print(tstat.percentile(a, 15))
+    # 6.4
+    print(tstat.percentile(a, 20))
+    # 8.2
+    print('*' * 25)
+
     print(tstat.var(a))
     # Expected result: 12279.439999999999
     print(tstat.var(b))
@@ -109,3 +119,10 @@ if __name__ == "__main__":
     # Expected result: 110.81263465868862
     print(tstat.std(b))
     # Expected result: 36.72533185690771
+    
+    print('*' * 25)
+    c = np.array(range(101))
+    print(tstat.percentile(c, 50))
+    print(tstat.percentile(c, 25))
+    print(tstat.mean(c))
+    print(tstat.median(c))
---
+++ module00/ex03/tools.py
@@ -0,0 +1,46 @@
+'''a function which adds an extra column of 1's on the left side of a given vector
+or matrix'''
+
+
+import numpy as np
+
+
+def add_intercept(x):
+    """Adds a column of 1's to the non-empty numpy.array x.
+    Args:
+    x: has to be a numpy.array of dimension m * n.
+    Returns:
+    X, a numpy.array of dimension m * (n + 1).
+    None if x is not a numpy.array.
+    None if x is an empty numpy.array.
+    Raises:
+    This function should not raise any Exception.
+    """
+    if not isinstance(x, np.ndarray) or not np.any(x):
+        return None
+
+    new_col = np.empty((x.shape[0], 1))
+    np.ndarray.fill(new_col, 1.)
+
+    if len(x.shape) == 1:
+        return np.concatenate(
+            [new_col, np.array(list([item] for item in x))], axis=1)
+    return np.concatenate([new_col, x], axis=1)
+
+
+if __name__ == "__main__":
+    x = np.arange(1, 6)
+    print(repr(add_intercept(x)))
+    # Output:
+    # array([[1., 1.],
+    # [1., 2.],
+    # [1., 3.],
+    # [1., 4.],
+    # [1., 5.]])
+    # Example 2:
+    y = np.arange(1, 10).reshape((3, 3))
+    print(repr(add_intercept(y)))
+    # Output:
+    # array([[1., 1., 2., 3.],
+    # [1., 4., 5., 6.],
+    # [1., 7., 8., 9.]])
