24448d8795e285ddd861f4a9fc4a83139ba8c2c8
ababoum <ababou237@hotmail.com>
2023-01-23 20:47:46 +0100

    being day2

---
+++ module02/ex00/prediction.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def simple_predict(x, theta):
+	"""Computes the prediction vector y_hat from two non-empty numpy.array.
+	Args:
+	x: has to be an numpy.array, a matrix of dimension m * n.
+	theta: has to be an numpy.array, a vector of dimension (n + 1) * 1.
+	Return:
+	y_hat as a numpy.array, a vector of dimension m * 1.
+	None if x or theta are empty numpy.array.
+	None if x or theta dimensions are not matching.
+	None if x or theta is not of expected type.
+	Raises:
+	This function should not raise any Exception.
+	"""
+	pass
+
+
+
+if __name__ == "__main__":
+	pass
